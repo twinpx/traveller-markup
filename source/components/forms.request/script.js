7099aabb3b750184a8bdd6523b1b906568ede24f
Tatiana <djembuka@gmail.com>
2016-06-07 17:24:25 +0300

    Request form page.

---
+++ source/components/forms.request/script.js
@@ -0,0 +1,17 @@
+( function($) {
+
+  'use strict';
+  
+  $( function() {
+  
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+  
+    /*if ( window.BX ) {
+      BX.addCustomEvent( "onFrameDataReceived", function () {});
+    }*/
+  });
+
+}( jQuery ));
